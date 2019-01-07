e97d0bd17ba10cd26bb6e63625a2c2eec4debd59
scc <928004235@qq.com>
2019-01-07 15:42:07 +0800

    结构修改，bug修改，样式调整首页

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -9,13 +9,18 @@
 // | Author: 老猫 <thinkcmf@126.com>
 // +----------------------------------------------------------------------
 namespace app\portal\controller;
-
+use app\portal\service\PostService;
 use cmf\controller\HomeBaseController;
 
 class IndexController extends HomeBaseController
 {
     public function index()
     {
+        $postService         = new PostService();
+        $case_list = $postService->publishedArticle(0,0);
+        $new_list = $postService->publishedArticle(0,0);
+        $this->assign('caseList', $case_list);
+        $this->assign('newsList', $new_list);
         return $this->fetch(':index');
     }
 }
