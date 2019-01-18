1cf5c36160afd82963cf50beaa693213ea01c59e
pfan8 <fqwgc8@outlook.com>
2019-01-18 22:53:54 +0800

    列表页，详情页完成

--- app/portal/service/PostService.php
+++ app/portal/service/PostService.php
@@ -99,6 +99,68 @@ class PostService
 
     }
 
+    public function ajaxListAction(){
+        $page = request()->param('apage');
+        if (!empty($page)) {
+            $join = [
+                ['__USER__ u', 'a.user_id = u.id']
+            ];
+
+            $field = 'a.*,u.user_login,u.user_nickname,u.user_email';
+
+            $category = empty($filter['category']) ? 0 : intval($filter['category']);
+            if (!empty($category)) {
+                array_push($join, [
+                    '__PORTAL_CATEGORY_POST__ b', 'a.id = b.post_id'
+                ]);
+                $field = 'a.*,b.id AS post_category_id,b.list_order,b.category_id,u.user_login,u.user_nickname,u.user_email';
+            }
+
+            $portalPostModel = new PortalPostModel();
+            $articles   = $portalPostModel->alias('a')->field($field)
+                ->join($join)
+                ->where('a.create_time', '>=', 0)
+                ->where('a.delete_time', 0)
+                ->where(function (Query $query) use ($filter, $isPage) {
+
+                    $category = empty($filter['category']) ? 0 : intval($filter['category']);
+                    if (!empty($category)) {
+                        $query->where('b.category_id', $category);
+                    }
+
+                    $startTime = empty($filter['start_time']) ? 0 : strtotime($filter['start_time']);
+                    $endTime   = empty($filter['end_time']) ? 0 : strtotime($filter['end_time']);
+                    if (!empty($startTime)) {
+                        $query->where('a.published_time', '>=', $startTime);
+                    }
+                    if (!empty($endTime)) {
+                        $query->where('a.published_time', '<=', $endTime);
+                    }
+
+                    $keyword = empty($filter['keyword']) ? '' : $filter['keyword'];
+                    if (!empty($keyword)) {
+                        $query->where('a.post_title', 'like', "%$keyword%");
+                    }
+
+                    if ($isPage) {
+                        $query->where('a.post_type', 2);
+                    } else {
+                        $query->where('a.post_type', 1);
+                    }
+                })
+                ->order('update_time', 'DESC')
+                ->paginate(10,false,[
+                    'type'     => 'Bootstrap',
+                    'var_page' => 'page',
+                    'page' => $page,
+                    'path'=>'javascript:AjaxPage([PAGE]);',
+
+                ]);
+            $page = $articles->render();
+        }
+        return json(['list'=>$articles,'page'=>$page]);
+    }
+
     /**
      * 已发布文章查询
      * @param  int $postId     文章id
