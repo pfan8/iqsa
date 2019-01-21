a5833aacf36a9849874a50148d95e59424e249fd
pfan8 <fqwgc8@outlook.com>
2019-01-21 20:40:42 +0800

    自助报价

---
+++ app/portal/controller/OfferController.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace app\portal\controller;
+use cmf\controller\HomeBaseController;
+
+class OfferController extends HomeBaseController
+{
+    public function index()
+    {
+        return $this->fetch(':offer');
+    }
+}
